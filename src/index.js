fd4fda143a25cb768280ea8c39a7fab863205c14
Matt Mundell <matt@mundell.me>
2024-09-15 02:22:41 +0200

    indent,fold asm

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ let props, data, parser
 props = [ indentNodeProp.add({ "InitList Block ErrBlock SwitchBlock ContainerBlock": delimitedIndent({ closing: '}' }),
                                'ParamDeclList FnCallArgs': delimitedIndent({ closing: ')',
                                                                              align: true }),
+                               AsmParams: delimitedIndent({ closing: ')',
+                                                            align: false }),
+                               "AsmOutList AsmInList AsmClobList": context => context.baseIndent + 2, // ': '
                                SwitchProng: context => context.baseIndent + context.unit,
                                IfStatement: continuedIndent({ except: /^\s*({|else\b)/ }),
                                MultiStringLiteral: continuedIndent(),
                                WhileStatement: continuedIndent() }),
-          foldNodeProp.add({ "InitList Block ErrBlock SwitchBlock ContainerBlock ParamDeclList FnCallArgs": foldInside }) ]
+          foldNodeProp.add({ "InitList Block ErrBlock SwitchBlock ContainerBlock ParamDeclList FnCallArgs AsmParams": foldInside }) ]
 
 data = { commentTokens: { line: "//" },
          closeBrackets: { brackets: ['(', '[', '{', "'", '"' ]}}
