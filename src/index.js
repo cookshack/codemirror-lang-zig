988acfb769baab8cc91d21fbd6a8b7087361531b
Matt Mundell <matt@mundell.me>
2024-09-16 01:14:59 +0200

    skip compl in some tokens

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import * as Grammar from './syntax.grammar'
 import { LRLanguage, LanguageSupport, indentNodeProp, continuedIndent, delimitedIndent, foldNodeProp, foldInside } from '@codemirror/language'
-import { completeFromList } from '@codemirror/autocomplete'
+import { completeFromList, ifNotIn } from '@codemirror/autocomplete'
 
-let props, data, parser, keywords, completionSource
+let props, data, parser, keywords, skipComplete, completionSource
 
 keywords = [ 'addrspace', 'align', 'allowzero', 'and', 'anyframe', 'anytype', 'asm', 'async', 'await', 'break', 'c', 'callconv', 'catch',
              'comptime', 'const', 'continue', 'defer', 'else', 'enum', 'errdefer', 'error', 'export', 'extern', 'fn', 'for', 'if', 'inline',
              'linksection', 'noalias', 'noinline', 'nosuspend', 'opaque', 'or', 'orelse', 'packed', 'pub', 'resume', 'return', 'struct',
              'suspend', 'switch', 'test', 'threadlocal', 'try', 'union', 'unreachable', 'usingnamespace', 'var', 'volatile', 'while' ]
 
-completionSource = completeFromList(keywords.map(kw => ({ label: kw, type: 'keyword' })))
+skipComplete = [ 'ContainerDocComment', 'DocComment', 'FieldAccess', 'LineComment', 'StringLiteral', 'StringLiteralSingle' ]
+
+completionSource = ifNotIn(skipComplete,
+                           completeFromList(keywords.map(kw => ({ label: kw, type: 'keyword' }))))
 
 props = [ indentNodeProp.add({ 'InitList Block ErrBlock SwitchBlock ContainerBlock': delimitedIndent({ closing: '}' }),
                                'ParamDeclList FnCallArgs': delimitedIndent({ closing: ')',
