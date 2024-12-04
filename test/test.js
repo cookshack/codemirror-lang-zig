db72ce17f3c97c9f5f54992647cfcb8f338607bf
Matt Mundell <matt@mundell.me>
2024-12-04 15:54:18 +0200

    format

--- test/test.js
+++ test/test.js
@@ -1,28 +1,32 @@
-import {lr} from "../dist/index.js"
-import {fileTests} from "@lezer/generator/dist/test"
+import { lr } from '../dist/index.js'
+import { fileTests } from '@lezer/generator/dist/test'
 
-import * as fs from "fs"
-import * as path from "path"
+import * as fs from 'fs'
+import * as path from 'path'
 import { fileURLToPath } from 'url'
-let caseDir = path.dirname(fileURLToPath(import.meta.url))
+let caseDir
 
 function test1(file) {
-  let name = /^[^\.]*/.exec(file)[0]
-  describe(name, () => {
+  let name
+
+  name = /^[^\.]*/.exec(file)[0]
+  globalThis.describe(name, () => {
     let content
-    content = fs.readFileSync(path.join(caseDir, file), "utf8")
+
+    content = fs.readFileSync(path.join(caseDir, file), 'utf8')
     //console.log(content)
-    for (let {name, run} of fileTests(content, file))
-      it(name, () => run(lr.parser))
+    for (let { name, run } of fileTests(content, file))
+      globalThis.it(name, () => run(lr.parser))
   })
 }
 
 function testAll() {
-  for (let file of fs.readdirSync(caseDir)) {
-    if (!/\.txt$/.test(file)) continue
-    test1(file)
-  }
+  for (let file of fs.readdirSync(caseDir))
+    if (/\.txt$/.test(file))
+      test1(file)
 }
+
+caseDir = path.dirname(fileURLToPath(import.meta.url))
 
 //console.log(process.argv)
 //console.log('single: ' + process.env.npm_config_singletest)
